f1ee0bc235d751f74b1cdb8ac32b052b85bb7b58
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2021-02-13 00:00:50 +0200

    only sender can access it

--- 02_peopleCount.sol
+++ 02_peopleCount.sol
@@ -3,12 +3,23 @@ pragma solidity ^0.5.1;
 contract MyConstract{
     
     uint256 public peopleCount = 0;
-    mapping(uint256 => Person) public people;   
+    mapping(uint256 => Person) public people;
+    
+     address owner;
+    
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    } 
     
     struct Person{
         uint _id;
         string _firstName;
         string _secondName;
+    }
+    
+    constructor() public{
+       owner == msg.sender;
     }
     
     function addPerson(string memory _firstName, string memory _secondName) public{
