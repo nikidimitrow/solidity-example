2000af287046320e59d20065865860761b3cdf54
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2021-02-16 22:53:15 +0200

    adding data type and event

--- 07_Coin.sol
+++ 07_Coin.sol
@@ -4,4 +4,11 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Coin{
 
+    address public minter;
+    mapping (address => uint) public balances;
+    
+    event Sent(address from, address to, uint amount);
+    
+  
+
 }
