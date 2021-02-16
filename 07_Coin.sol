c5121eff9d7e3afcdcc6510e8b5d232ba957ba63
Nikolay Dimitrow <nikidimitrow@users.noreply.github.com>
2021-02-16 22:53:40 +0200

    adding function

--- 07_Coin.sol
+++ 07_Coin.sol
@@ -4,11 +4,21 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Coin{
 
-    address public minter;
-    mapping (address => uint) public balances;
+  address public minter;
+  mapping (address => uint) public balances;
     
-    event Sent(address from, address to, uint amount);
+  event Sent(address from, address to, uint amount);
     
-  
+  function mint(address receiver, uint amount) public {
+        if (msg.sender != minter) return;
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint amount) public {
+        if (balances[msg.sender] < amount) return;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
 
 }
